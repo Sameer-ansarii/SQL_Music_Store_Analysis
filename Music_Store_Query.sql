26bca2c1290dc36077c5629438403c42fbe8c95b
Sameer Ansari <125865393+Sameer-ansarii@users.noreply.github.com>
2023-07-16 16:57:30 +0545

    Add files via upload

--- Music_Store_Query.sql
+++ Music_Store_Query.sql
@@ -21,7 +21,8 @@ FROM invoice
 ORDER BY total DESC
 
 
---Q4:  Which city has the best customers? We would like to throw a promotional Music Festival in the city we made the most money. Write a query that returns one city that has the highest sum of invoice totals. Return both the city name & sum of all invoice totals.
+--Q4:  Which city has the best customers? We would like to throw a promotional Music Festival in the city we made the most money. Write a query that returns one city that has the highest sum of invoice totals.
+--Return both the city name & sum of all invoice totals.
 
 SELECT billing_city,SUM(total) AS InvoiceTotal
 FROM invoice
@@ -92,7 +93,9 @@ ORDER BY miliseconds DESC;
 
 --Q9:  Find how much amount spent by each customer on artists? Write a query to return customer name, artist name and total spent.
 
---Steps to Solve: First, find which artist has earned the most according to the InvoiceLines. Now use this artist to find which customer spent the most on this artist. For this query, you will need to use the Invoice, InvoiceLine, Track, Customer, Album, and Artist tables. Note, this one is tricky because the Total spent in the Invoice table might not be on a single product, so you need to use the InvoiceLine table to find out how many of each product was purchased, and then multiply this by the price for each artist.
+--Steps to Solve: First, find which artist has earned the most according to the InvoiceLines. Now use this artist to find which customer spent the most on this artist. 
+--For this query, you will need to use the Invoice, InvoiceLine, Track, Customer, Album, and Artist tables. Note, this one is tricky because the Total spent in the Invoice 
+--table might not be on a single product, so you need to use the InvoiceLine table to find out how many of each product was purchased, and then multiply this by the price for each artist.
 
 WITH best_selling_artist AS (
 	SELECT artist.artist_id AS artist_id, artist.name AS artist_name, SUM(invoice_line.unit_price*invoice_line.quantity) AS total_sales
@@ -115,7 +118,8 @@ GROUP BY 1,2,3,4
 ORDER BY 5 DESC;
 
 
---Q10:  We want to find out the most popular music Genre for each country. We determine the most popular genre as the genre with the highest amount of purchases. Write a query that returns each country along with the top Genre. For countries where the maximum number of purchases is shared return all Genres.
+--Q10:  We want to find out the most popular music Genre for each country. We determine the most popular genre as the genre with the highest amount of purchases. Write a query that returns each country along with the top Genre.
+--For countries where the maximum number of purchases is shared return all Genres.
 
 --Steps to Solve:  There are two parts in question- first most popular music genre and second need data at country level.
 
@@ -134,7 +138,8 @@ WITH popular_genre AS
 SELECT * FROM popular_genre WHERE RowNo <= 1
 
 
---Q11: Write a query that determines the customer that has spent the most on music for each country. Write a query that returns the country along with the top customer and how much they spent. For countries where the top amount spent is shared, provide all customers who spent this amount.
+--Q11: Write a query that determines the customer that has spent the most on music for each country. Write a query that returns the country along with the top customer and how much they spent. 
+--For countries where the top amount spent is shared, provide all customers who spent this amount.
 
 --Steps to Solve:  Similar to the above question. There are two parts in question- first find the most spent on music for each country and second filter the data for respective customers.
 
